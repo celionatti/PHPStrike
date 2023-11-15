52173c84496f286153fb16090aa6905ae63b4eb9
celionatti <amisuusman@gmail.com>
2023-11-15 01:32:11 +0100

    adjustment

--- utils/functions.php
+++ utils/functions.php
@@ -15,27 +15,23 @@ function isLoggedIn()
     return BoltAuthentication::currentUser();
 }
 
-function getSingleData($dataKey)
+function getSingleData($data, $dataKey)
 {
-    $user = isLoggedIn();
-
-    if ($user && isset($user->$dataKey)) {
-        return $user->$dataKey;
+    if ($data && isset($data->$dataKey)) {
+        return $data->$dataKey;
     } else {
         return null;
     }
 }
 
-function getCombinedData(...$dataKeys)
+function getCombinedData($data, ...$dataKeys)
 {
-    $user = isLoggedIn();
-
-    if ($user) {
+    if ($data) {
         $combinedData = '';
 
         foreach ($dataKeys as $dataKey) {
-            if (isset($user->$dataKey)) {
-                $combinedData .= $user->$dataKey . ' ';
+            if (isset($data->$dataKey)) {
+                $combinedData .= $data->$dataKey . ' ';
             } else {
                 return null; // Return null if any of the keys is not set.
             }
@@ -44,5 +40,15 @@ function getCombinedData(...$dataKeys)
         return trim($combinedData); // Remove trailing space and return the combined data.
     } else {
         return null;
+    }
+}
+
+// Function to generate an HTML string based on user verification status
+function verificationStatus($verificationStatus)
+{
+    if ($verificationStatus == 1) {
+        return '<span style="color: green;">Verified</span>';
+    } else {
+        return '<span style="color: red;">Not Verified</span>';
     }
 }
